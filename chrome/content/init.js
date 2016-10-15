b1d761087e18a3b867407da2bcbb2149e09d6c4e
Kesantielu Dasefern <dasefern@gmail.com>
2016-10-15 12:35:49 +0600

    Update init.js

--- chrome/content/init.js
+++ chrome/content/init.js
@@ -3,17 +3,19 @@ var styles = [
     {css: 'friends', match: 'friends'}
 ];
 
-var vkinfo = document.createElement('script');
-vkinfo.setAttribute('type', 'text/javascript');
-vkinfo.innerHTML = 'function vkwait(){if(typeof vk!=="undefined"){window.postMessage({type:"VK_INFO",text:vk.lang},"*");}else{setTimeout(function(){vkwait();},10)}}vkwait();';
+var langMap = {0: 'ru', 1: 'uk', 2: 'be-tarask', 3: 'en-us', 114: 'be', 100: 'ru-petr1708', 777: 'ru-ussr'};
+
+var vkInfo = document.createElement('script');
+vkInfo.setAttribute('type', 'text/javascript');
+vkInfo.innerHTML = 'function vkwait(){if(typeof vk!=="undefined"){window.postMessage({type:"VK_INFO",text:vk.lang},"*");}else{setTimeout(function(){vkwait();},10)}}vkwait();';
 
 document.arrive('head', function () {
-
     chrome.storage.local.get('enabled', function (item) {
         if (item.enabled) {
             checkCSS(styles);
+            insertCSS('local');
             insertCSS('main');
-            document.head.appendChild(vkinfo);
+            document.head.appendChild(vkInfo);
             chrome.runtime.sendMessage({action: 'activating'}, null);
             chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                 if (sender.id == chrome.runtime.id && request.action == 'updating') {
@@ -21,52 +23,37 @@ document.arrive('head', function () {
                 }
             });
             document.arrive('#side_bar_inner', function () {
-                $('<li id="l_ntf" class=""><a href="/feed?section=notifications" class="left_row"><span class="left_fixer"><span class="left_count_wrap fl_r left_void"><span class="inl_bl left_count_sign"></span></span><span class="left_icon fl_l"></span><span class="left_label inl_bl">Мои Ответы</span></span></a></li>').insertAfter('#l_nwsf')
-
+                $('<li id="l_ntf" class=""><a href="/feed?section=notifications" class="left_row"><span class="left_fixer"><span class="left_count_wrap fl_r left_void"><span class="inl_bl left_count_sign"></span></span><span class="left_icon fl_l"></span><span class="left_label inl_bl">Ответы</span></span></a></li>').insertAfter('#l_nwsf')
             });
         }
     });
 });
 
-
 window.addEventListener('message', function (event) {
     if (event.source == window && event.data.type && event.data.type == 'VK_INFO') {
-        console.log('Language: ' + event.data.text);
-        localizeCSS(event.data.text);
+        var lang = event.data.text;
+        console.log('Language: ' + lang);
+        document.documentElement.setAttribute('lang', langMap[lang]);
     }
 }, false);
-
-
-
 
 window.addEventListener('onbeforeunload', function () {
     Arrive.unbindAllArrive();
 });
 
-function insertCSS(style, body) {
-    if (body === undefined) body = false;
+function insertCSS(style) {
     var css = chrome.extension.getURL('content/' + style + '.css');
     if ($('#oldvk-style-' + style).length == 0) {
         var link = '<link rel="stylesheet" type="text/css" href="' + css + '" id="oldvk-style-' + style + '"/>';
-        if (body) $(link).insertBefore('body'); else $(link).insertAfter('head');
+        $(link).insertAfter('head');
         console.log(style + '.css inserted');
     }
 }
 
-function removeCSS(style) {
-    var name = $('#oldvk-style-' + style);
-    if (name.length != 0) {
-        name.remove();
-        console.log(style + '.css removed');
-    }
-}
-
-function updateCSS(styles, body) {
-    if (body === undefined) body = true;
+function updateCSS(styles) {
     styles.forEach(function (style) {
-        if (style.apply) {
-            insertCSS(style.css, body)
-        } else removeCSS(style.css)
+        if (style.apply) document.head.classList.add('oldvk-' + style.css);
+        else document.head.classList.remove('oldvk-' + style.css);
     })
 }
 
@@ -80,38 +67,4 @@ function checkCSS(styles) {
         Styles.push({css: style.css, apply: apply})
     });
     updateCSS(Styles, false)
-}
-
-function localizeCSS(lang) {
-    var my = '';
-    var css = '#l_nwsf .left_label:before{content:"$my"}#l_msg .left_label:before{content:"$my"}#l_fr .left_label:before{content:"$my"}#l_gr .left_label:before{content:"$my"}#l_ph .left_label:before{content:"$my"}#l_aud .left_label:before{content:"$my"}#l_vid .left_label:before{content:"$my"}#l_ap .left_label:before{content:"$my"}#l_fav .left_label:before{content:"$my"}#l_doc .left_label:before{content:"$my"}';
-    switch (lang) {
-        case 0:
-            my = 'Мои ';
-            break;
-        case 1:
-            my = 'Мої ';
-            break;
-        case 3:
-            my = 'My ';
-            break;
-        case 2:
-        case 114:
-            my = 'Mае ';
-            break;
-        default:
-            console.log('Этот язык пока не поддерживается: ' + lang);
-            break;
-    }
-    css = css.replace(/\$my/g, my);
-    applyCSS(css, 'my');
-}
-
-function applyCSS(css, name) {
-    if ($('#oldvk-style1-' + name).length == 0) {
-        var style = document.createElement('style');
-        style.id = 'oldvk-style1-' + name;
-        style.innerHTML = css;
-        document.head.appendChild(style);
-    }
 }
