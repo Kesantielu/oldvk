06d98b8f389466d201cc30de0338dda37eb9c43d
Kesantielu Dasefern <dasefern@gmail.com>
2016-10-15 12:33:14 +0600

    Update events.js

--- chrome/events.js
+++ chrome/events.js
@@ -3,25 +3,39 @@ var styles = [
     {css: 'friends', match: 'friends'}
 ];
 
+if (!String.prototype.startsWith) {
+    Object.defineProperty(String.prototype, 'startsWith', {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: function (searchString, position) {
+            position = position || 0;
+            return this.lastIndexOf(searchString, position) === position;
+        }
+    });
+}
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    function listener(details) {
-        console.log(details);
-        var url = document.createElement('a');
-        url.href = details.url;
-        var path = url.pathname.slice(1);
-        var Styles = [];
-        styles.forEach(function (style) {
-            var apply = !!path.startsWith(style.match);
-            Styles.push({css: style.css, apply: apply})
-        });
-        chrome.tabs.sendMessage(details.tabId, {action: 'updating', css: Styles}, null)
+    function listener(tabid, info, tab) {
+        console.log(info);
+        if (info.status == 'loading') {
+            var url = document.createElement('a');
+            url.href = info.url;
+            var path = url.pathname.slice(1);
+            var Styles = [];
+            styles.forEach(function (style) {
+                var apply = !!path.startsWith(style.match);
+                Styles.push({css: style.css, apply: apply})
+            });
+            chrome.tabs.sendMessage(tabid, {action: 'updating', css: Styles}, null)
+        }
     }
 
     if (request.action == 'activating') {
-        chrome.webNavigation.onHistoryStateUpdated.addListener(listener, {url: [{hostEquals: 'vk.com'}]});
+        chrome.tabs.onUpdated.addListener(listener);
     }
 });
 
 chrome.runtime.onInstalled.addListener(function () {
-   chrome.storage.local.set({enabled:true})
+    chrome.storage.local.set({enabled: true})
 });
