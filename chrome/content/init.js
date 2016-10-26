31d6b174e93a6fde4b15ab91c1034b7e12c27401
Kesantielu Dasefern <dasefern@gmail.com>
2016-10-26 11:43:34 +0600

    Исправления: * Отваливающийся EventListener * Отвязка от jQuery за ненадобностью * Мелкие исправления

--- chrome/content/init.js
+++ chrome/content/init.js
@@ -5,25 +5,44 @@ var styles = [
 
 var langMap = {0: 'ru', 1: 'uk', 2: 'be-tarask', 3: 'en-us', 114: 'be', 100: 'ru-petr1708', 777: 'ru-ussr'};
 
+if (!String.prototype.startsWith) {
+    Object.defineProperty(String.prototype, 'startsWith', {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: function (searchString, position) {
+            position = position || 0;
+            return this.lastIndexOf(searchString, position) === position;
+        }
+    });
+}
+
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
 var vkInfo = document.createElement('script');
 vkInfo.setAttribute('type', 'text/javascript');
 vkInfo.innerHTML = 'function vkwait(){if(typeof vk!=="undefined"){window.postMessage({type:"VK_INFO",text:vk.lang},"*");}else{setTimeout(function(){vkwait();},10)}}vkwait();';
 
-document.arrive('head', function () {
+document.arrive('head', {onceOnly: true, existing: true}, function () {
     chrome.storage.local.get('enabled', function (item) {
         if (item.enabled) {
             checkCSS(styles);
             insertCSS('local');
             insertCSS('main');
             document.head.appendChild(vkInfo);
-            chrome.runtime.sendMessage({action: 'activating'}, null);
             chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                 if (sender.id == chrome.runtime.id && request.action == 'updating') {
                     updateCSS(request.css)
                 }
             });
+            var l_ntf = document.createElement('li');
+            l_ntf.id = 'l_ntf';
+            l_ntf.innerHTML = '<a href="/feed?section=notifications" class="left_row"><span class="left_fixer"><span class="left_count_wrap fl_r left_void"><span class="inl_bl left_count_sign"></span></span><span class="left_icon fl_l"></span><span class="left_label inl_bl">Ответы</span></span></a>';
             document.arrive('#side_bar_inner', function () {
-                $('<li id="l_ntf" class=""><a href="/feed?section=notifications" class="left_row"><span class="left_fixer"><span class="left_count_wrap fl_r left_void"><span class="inl_bl left_count_sign"></span></span><span class="left_icon fl_l"></span><span class="left_label inl_bl">Ответы</span></span></a></li>').insertAfter('#l_nwsf')
+                if (document.getElementById('l_nwsf'))
+                    insertAfter(document.getElementById('l_nwsf'), l_ntf)
             });
         }
     });
@@ -37,15 +56,19 @@ window.addEventListener('message', function (event) {
     }
 }, false);
 
-window.addEventListener('onbeforeunload', function () {
+window.addEventListener('beforeunload', function () {
     Arrive.unbindAllArrive();
 });
 
 function insertCSS(style) {
     var css = chrome.extension.getURL('content/' + style + '.css');
-    if ($('#oldvk-style-' + style).length == 0) {
-        var link = '<link rel="stylesheet" type="text/css" href="' + css + '" id="oldvk-style-' + style + '"/>';
-        $(link).insertAfter('head');
+    if (!document.getElementById('oldvk-style-' + style)) {
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = css;
+        link.id = 'oldvk-style-' + style;
+        insertAfter(document.head, link);
         console.log(style + '.css inserted');
     }
 }
--- chrome/events.js
+++ chrome/events.js
@@ -14,27 +14,21 @@ if (!String.prototype.startsWith) {
         }
     });
 }
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    function listener(tabid, info, tab) {
-        console.log(info);
-        if (info.status == 'loading') {
-            var url = document.createElement('a');
-            url.href = info.url;
-            var path = url.pathname.slice(1);
-            var Styles = [];
-            styles.forEach(function (style) {
-                var apply = !!path.startsWith(style.match);
-                Styles.push({css: style.css, apply: apply})
-            });
-            chrome.tabs.sendMessage(tabid, {action: 'updating', css: Styles}, null)
-        }
+function listener(tabId, info, tab) {
+    if (info.status == 'loading') {
+        var url = document.createElement('a');
+        url.href = info.url;
+        var path = url.pathname.slice(1);
+        var Styles = [];
+        styles.forEach(function (style) {
+            var apply = !!path.startsWith(style.match);
+            Styles.push({css: style.css, apply: apply})
+        });
+        chrome.tabs.sendMessage(tabId, {action: 'updating', css: Styles}, null)
     }
+}
 
-    if (request.action == 'activating') {
-        chrome.tabs.onUpdated.addListener(listener);
-    }
-});
+chrome.tabs.onUpdated.addListener(listener);
 
 chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.local.set({enabled: true})
