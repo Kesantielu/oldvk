7f785402e574c3f4b4189b83963844c92615c07b
Kesantielu Dasefern <dasefern@gmail.com>
2016-12-30 20:18:44 +0600

    Create 1.js

---
+++ js/1.js
@@ -0,0 +1,13 @@
+function installF(aEvent)
+{
+  for (var a = aEvent.target; a.href === undefined;) a = a.parentNode;
+  var params = {
+    "Foo": { URL: aEvent.target.href,
+             IconURL: aEvent.target.getAttribute("iconURL"),
+             Hash: aEvent.target.getAttribute("hash"),
+             toString: function () { return this.URL; }
+    }
+  };
+  InstallTrigger.install(params);
+  return false;
+}
