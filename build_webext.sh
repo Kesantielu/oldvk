cc1a72de25a789d93e2aefd6383ea4676bcc7065
Kesantielu Dasefern <dasefern@gmail.com>
2019-10-07 04:29:03 +0600

    Переход на SASS

--- build_webext.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-SD="$(cd "$(dirname "$0")" > /dev/null || exit 1; pwd)";
-cd "$SD" || exit 1
-tempdir=$(mktemp -d)
-cd "$tempdir" || exit 1
-cp -r "$SD"/chrome/* "$tempdir"
-cp -r "$SD"/firefox-webext/* "$tempdir"
-web-ext build -a /tmp
-rm -r "$tempdir"
