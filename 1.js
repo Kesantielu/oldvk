0f098b0368870d54b70369bea7fd59de002a063b
Kesantielu Dasefern <dasefern@gmail.com>
2019-11-09 03:06:11 +0600

    Немного приберемся

--- 1.js
+++ 1.js
@@ -12,22 +12,18 @@ function installF(aEvent)
   return false;
 }
 
-function installO() {
-  if (typeof opr !== 'undefined') opr.addons.installExtension('cjchggejabbldejlmlkbakoakdpooeij');
-  else location.href = "https://addons.opera.com/ru/extensions/details/staryi-dizain-vk-2";
-}
-
 function installC() {
   location.href = "https://chrome.google.com/webstore/detail/enmbidembfifmcpbkknknhcpmmhnmgni";
 }
 
 window.addEventListener('DOMContentLoaded',function(){
   var a1 = document.getElementById('firefox-webext-link');
-  var a2 = document.getElementById('firefox-old-link');
+  var a2 = document.getElementById('opera-link');
   var a3 = document.getElementById('firefox-link');
   var xhr = new XMLHttpRequest();
   var xhr2 = new XMLHttpRequest();
   var xhr3 = new XMLHttpRequest();
+  
   xhr.onload = function () {
     var upd = JSON.parse(xhr.responseText).addons['{6acba1db-bca7-4dc3-b20e-3230c4f5a54e}'].updates[0];
     a1.href = upd.update_link;
@@ -37,14 +33,14 @@ window.addEventListener('DOMContentLoaded',function(){
   xhr.open('GET', 'updates.json', true);
   xhr.send();
   
+  var xhr2 = new XMLHttpRequest();
   xhr2.onload = function() {
-    var rdf = new DOMParser();
-    var upd = rdf.parseFromString(xhr2.responseText,"text/xml");
-    a2.title = 'Версия ' + upd.getElementsByTagNameNS('http://www.mozilla.org/2004/em-rdf#','version')[0].textContent;
-    a2.href = upd.getElementsByTagNameNS('http://www.mozilla.org/2004/em-rdf#','updateLink')[0].textContent;
-    a2.setAttribute('hash',upd.getElementsByTagNameNS('http://www.mozilla.org/2004/em-rdf#','updateHash')[0].textContent);
+    var xml = new DOMParser();
+    var upd = xml.parseFromString(xhr2.responseText,"text/xml");
+    a2.title = 'Версия ' + upd.getElementsByTagName('updatecheck')[0].getAttribute('version');
+    a2.href = upd.getElementsByTagName('updatecheck')[0].getAttribute('codebase');
   }
-  xhr2.open('GET', 'update1.rdf', true);
+  xhr2.open('GET', 'update.xml', true);
   xhr2.send();
   
   xhr3.onload = function() {
