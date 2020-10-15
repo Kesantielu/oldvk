8553404f6365748ac1f786c648f84f0ea08a5ea6
Kesantielu Dasefern <dasefern@gmail.com>
2020-10-15 14:53:23 +0600

    Повышение минимальной версии Chrome до 45, исключение ненужных полифиллов

--- chrome/lib/lib.js
+++ chrome/lib/lib.js
@@ -1,4 +1,4 @@
-if (!window.browser)
+﻿if (!window.browser)
     window.browser = window.msBrowser || window.chrome;
 
 var isFirefox = typeof InstallTrigger !== 'undefined';
@@ -13,76 +13,9 @@ if (isFirefox && !isWebExt) {
     })
 }
 
-if (!String.prototype.startsWith) {
-    Object.defineProperty(String.prototype, 'startsWith', {
-        enumerable: false,
-        configurable: false,
-        writable: false,
-        value: function (searchString, position) {
-            position = position || 0;
-            return this.lastIndexOf(searchString, position) === position;
-        }
-    });
-}
-
-if (!String.prototype.endsWith) {
-    Object.defineProperty(String.prototype, 'endsWith', {
-        value: function (searchString, position) {
-            var subjectString = this.toString();
-            if (position === undefined || position > subjectString.length) {
-                position = subjectString.length;
-            }
-            position -= searchString.length;
-            var lastIndex = subjectString.indexOf(searchString, position);
-            return lastIndex !== -1 && lastIndex === position;
-        }
-    });
-}
-
 function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
-
-if (!Element.prototype.matches) {
-    Element.prototype.matches =
-        Element.prototype.matchesSelector ||
-        Element.prototype.webkitMatchesSelector ||
-        Element.prototype.mozMatchesSelector ||
-        Element.prototype.msMatchesSelector
-}
-
-if (!Object.assign) {
-    Object.defineProperty(Object, 'assign', {
-        enumerable: false,
-        configurable: true,
-        writable: true,
-        value: function (target, firstSource) {
-            'use strict';
-            if (target === undefined || target === null) {
-                throw new TypeError('Cannot convert first argument to object');
-            }
-
-            var to = Object(target);
-            for (var i = 1; i < arguments.length; i++) {
-                var nextSource = arguments[i];
-                if (nextSource === undefined || nextSource === null) {
-                    continue;
-                }
-
-                var keysArray = Object.keys(Object(nextSource));
-                for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
-                    var nextKey = keysArray[nextIndex];
-                    var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
-                    if (desc !== undefined && desc.enumerable) {
-                        to[nextKey] = nextSource[nextKey];
-                    }
-                }
-            }
-            return to;
-        }
-    });
-}
-
 
 function wait(condition, callback) {
     if (typeof condition() !== "undefined") {
@@ -370,5 +303,15 @@ var i18n = {
         114: 'Выдаліць',
         100: 'Сжечь',
         777: 'Сжечь'
+    },
+    messages: {
+        0: 'Сообщения',
+        1: 'Повідомлення',
+        2: 'Паведамленьні',
+        3: 'Messages',
+        97: 'Хабарламалар',
+        114: 'Паведамленні',
+        100: 'Письма',
+        777: 'Телеграммы'
     }
 };
